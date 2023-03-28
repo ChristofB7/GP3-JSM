054215b2e9e836369777a766f416a36b2e349ca1
christof <christof@jazab.com>
2023-03-28 14:08:37 -0700

    added Feature component, whatGPT3 section, features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './features.css'
+import { Feature } from '../../components'
+
+const featuresData = [
+    {
+        title: 'Write Code',
+        text: 'GPT-3 has the ability to write code in almost any language you request. It can build code from scratch or edit inserted code.'
+    },
+    {
+        title: 'Create Marketing Content',
+        text: 'GPT-3 will generate conent for your next eBook, or even with your next blog post. Having GPT in your pocket will elevate your content to the next level.'
+    },
+    {
+        title: 'General Research',
+        text: "Whether it's for school, business, or just growing your knowledge GPT-3 can help you learn. GPT-3 is an awesome tool for doing research or summarizing web content."
+    },
+    {
+        title: 'Create Fictional Stories',
+        text: 'Even with a vague prompt GPT-3 can come up with a full story with loveable characters and intruiging plot. We look forward to seeing your next netflix original.'
+    },
+
+]
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='gpt3__features section__padding' id='features'>
+            <div className='gpt3__features-heading'>
+                <h1 className='gradient__text'>
+                    The Future is Now and You Just Need To Realize It. Step into Future Today and Make it Happen.
+                </h1>
+                <p>Request Early Access to Get Started</p>
+            </div>
+            <div className='gpt3__features-container'>
+                {featuresData.map((item, index) => (
+                    <Feature title={item.title} text={item.text} key={item.title + index} />
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './whatGPT3.css'
 
 const WhatGPT3 = () => {
     return (
-        <div>WhatGPT3</div>
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature title='What is GPT3' text="We are excited to introduce ChatGPT to get users’ feedback and learn about its strengths and weaknesses. During the research preview, usage of GPT-3 is free." />
+            </div>
+
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+                <p>Explore the libraray</p>
+            </div>
+
+            <div className='gpt3__whatgpt3-container'>
+                <Feature title='Chatbots' text="GPT-3 can carry out humanlike conversations and write natural, fluid prose on an endless variety of topics. They can also perform complex tasks, from writing code to planning a kid’s birthday party." />
+                <Feature title='Knowledgebase' text="We preview GPT-3’s performance by evaluating it on a narrow suite of standard academic vision benchmarks. However, these numbers do not fully represent the extent of its capabilities as we are constantly discovering new and exciting tasks that the model is able to tackle." />
+                <Feature title='Education' text='We trained this model using Reinforcement Learning from Human Feedback (RLHF), using the same methods as InstructGPT, but with slight differences in the data collection setup. ' />
+            </div>
+        </div>
     )
 }
 
