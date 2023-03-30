44207c1d1c6f5281bbaa4353b692658412bc985b
christof <christof@jazab.com>
2023-03-30 12:02:29 -0700

    finished blog and footer sections

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
     return (
-        <div>Possibility</div>
+        <div className='gpt3__possibility section__padding' id='possibility'>
+            <div className='gpt3__possibility-image'>
+                <img src={possibilityImage} alt="possibilityImage" />
+            </div>
+            <div className='gpt3__possibility-content'>
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className='gradient__text'>The possbilities are beyond your imagination</h1>
+                <p>There’s a lot of things you can get GPT-3 to do. From fun things like getting ideas for decorations, party planning, and more, to more useful ChatGPT benefits like helping you debug your code, or even write code for you.</p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
+        </div>
     )
 }
 
